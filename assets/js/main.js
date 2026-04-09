179d3e5109760e62f5d998f8b936dc5ffb97f762
jqparnell <parnelldeplano@gmail.com>
2026-04-09 19:00:22 +0000

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
-let rotation =0;
-document.addEventListener("click", function () {
-    let min = 10;
-    let max= 40;
-    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
-    document.body.style.transform = `rotate(${rotation}deg)`;
-});
+let grid = document.querySelectorAll(".grid-container");
+let divs = document.querySelectorAll(".grid-container div");
+
+function randomize() {
+    console.log(divs.length, "randomize!")
+
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`;
+
+       divs.forEach(function(div){
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
+
+        if (Math.random() > .5) {
+            scale = 1; // don't affect scale
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
+        }
+
+         div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
+}
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
